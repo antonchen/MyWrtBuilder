98b99aac97dc368988e2763678f34d9a8babd2d2
Anton Chen <contact@antonchen.com>
2023-01-01 00:07:37 +0800

    update

--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -11,8 +11,9 @@ sed -i "/luci-app-cpufreq/d" include/target.mk
 # Force opkg to overwrite files
 sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrite/" Makefile
 
-# Not generate ISO images for it is too big
+# Not generate
 sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
-
-# Not generate VHDX images
+sed -i "s/CONFIG_QCOW2_IMAGES=y/# CONFIG_QCOW2_IMAGES is not set/" .config
+sed -i "s/CONFIG_VDI_IMAGES=y/# CONFIG_VDI_IMAGES is not set/" .config
+sed -i "s/CONFIG_VMDK_IMAGES=y/# CONFIG_VMDK_IMAGES is not set/" .config
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
