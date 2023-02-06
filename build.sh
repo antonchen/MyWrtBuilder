806abd7245a7ea92d5339e9df4a0d1cae99b56f9
Anton Chen <contact@antonchen.com>
2023-02-06 22:28:36 +0800

    bypass

--- build.sh
+++ build.sh
@@ -12,16 +12,13 @@ PACKAGES=""
 PACKAGES="$PACKAGES luci-theme-argonv3"
 
 # 代理
-PACKAGES="$PACKAGES luci-app-mosdns luci-i18n-mosdns-zh-cn"
 PACKAGES="$PACKAGES luci-app-passwall luci-i18n-passwall-zh-cn ipset"
 
 # 界面翻译补全
 PACKAGES="$PACKAGES luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-turboacc-zh-cn"
 
 # 常用服务
-PACKAGES="$PACKAGES ipv6helper luci-app-vlmcsd luci-app-upnp luci-i18n-upnp-zh-cn"
-# AdGuard Home
-PACKAGES="$PACKAGES adguardhome"
+PACKAGES="$PACKAGES ipv6helper luci-app-vlmcsd"
 
 # 不需要的包
 PACKAGES="$PACKAGES -luci-app-filetransfer -luci-app-cpufreq"
--- scripts/builder-patch.sh
+++ scripts/builder-patch.sh
@@ -10,7 +10,7 @@ sed -i "s/install \$(BUILD_PACKAGES)/install \$(BUILD_PACKAGES) --force-overwrit
 
 # Not generate
 sed -i "s/CONFIG_ISO_IMAGES=y/# CONFIG_ISO_IMAGES is not set/" .config
-sed -i "s/CONFIG_QCOW2_IMAGES=y/# CONFIG_QCOW2_IMAGES is not set/" .config
+# sed -i "s/CONFIG_QCOW2_IMAGES=y/# CONFIG_QCOW2_IMAGES is not set/" .config
 sed -i "s/CONFIG_VDI_IMAGES=y/# CONFIG_VDI_IMAGES is not set/" .config
 sed -i "s/CONFIG_VMDK_IMAGES=y/# CONFIG_VMDK_IMAGES is not set/" .config
 sed -i "s/CONFIG_VHDX_IMAGES=y/# CONFIG_VHDX_IMAGES is not set/" .config
